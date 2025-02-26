ceb898cab2ff10c4129e15925a92507793d279a5
Akash Gundapuneni <akashnaidu545@gmail.com>
2025-02-26 17:25:32 +0100

    Add another page to the routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,17 @@ import { Routes, Route } from "react-router";
 
 import Layout from "@/app/dashboard/page";
 import Testpage from "@/app/testpage/page";
+import Newpage from "@/app/newpage/page";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Testpage />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Testpage />} />
+      </Route>
+      <Route path="/newpage" element={<Newpage />} />
+    </Routes>
+  );
 }
 
 export default App;
